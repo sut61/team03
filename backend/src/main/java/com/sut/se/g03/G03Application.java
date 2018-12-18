193a131bd17078ce4801801d51eb415bf319d4ee
forze977 <ford_pachara@hotmail.com>
2018-12-18 18:16:53 +0700

    add server

---
+++ backend/src/main/java/com/sut/se/g03/G03Application.java
@@ -0,0 +1,14 @@
+package com.sut.se.g03.G03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G03Application.class, args);
+	}
+
+}
+
