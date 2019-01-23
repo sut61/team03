e02aad1b3bb7509d474e50dcc5af3e93f7fae0ba
forze977 <ford_pachara@hotmail.com>
2019-01-23 12:59:31 +0700

    edit RoomReserveController for room status

--- backend/src/main/java/com/sut/se/g03/repository/RoomRepository.java
+++ backend/src/main/java/com/sut/se/g03/repository/RoomRepository.java
@@ -3,6 +3,7 @@ package com.sut.se.g03.repository;
 import com.sut.se.g03.entity.Room;
 import com.sut.se.g03.entity.RoomSize;
 import com.sut.se.g03.entity.RoomType;
+import com.sut.se.g03.entity.StatusRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,4 +15,6 @@ import java.util.Optional;
 @CrossOrigin(origins = "http://localhost:4200")
 public interface RoomRepository extends JpaRepository<Room,Long> {
     Collection<Room> findByRoomSizeAndRoomType(Optional<RoomSize> roomSize, Optional<RoomType> roomType);
+    Collection<Room> findAllByRoomSizeAndRoomTypeAndStatusRoom(Optional<RoomSize> roomSize, Optional<RoomType> roomType,
+                                                               StatusRoom statusRoom);
 }
