ccaae65c1495b83e1f90fb0731d54e3feee8cd1b
paripatdr <paripatwork@gmail.com>
2019-01-25 14:42:03 +0700

    close #55 - Data Load Edit

--- backend/src/main/java/com/sut/se/g03/repository/BillRepository.java
+++ backend/src/main/java/com/sut/se/g03/repository/BillRepository.java
@@ -8,4 +8,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
 public interface BillRepository extends JpaRepository<Bill,Long> {
+
 }
