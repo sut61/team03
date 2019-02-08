8809ad8466b8daac764fee11a83e5b19993df957
meesss <mee_manassanan@hotmail.com>
2019-02-08 17:12:16 +0700

    close#60 - ทำ Entity

---
+++ backend/src/main/java/com/sut/se/g03/entity/CourseReserve.java
@@ -0,0 +1,56 @@
+package com.sut.se.g03.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@ToString
+@EqualsAndHashCode
+@Data
+public class CourseReserve {
+    @SequenceGenerator(name="coursereserve_seq",sequenceName="coursereserve_seq")
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="coursereserve_seq")
+    @Id
+    private Long id;
+
+    @NotNull
+    @Size(min = 11,max = 11)
+    @Pattern(regexp = "\\d{3}-\\d+")
+    private String phone;
+
+    @NotNull
+    @Size(min = 2,max = 15)
+    private String nickname;
+    
+   
+    @ManyToOne
+	private Member member;
+    
+    @NotNull
+    @ManyToOne
+    StatusCourse statusCourse;
+    
+    @NotNull
+    @ManyToOne
+    CourseMusic coursemusic;
+    
+
+    public CourseReserve(){}
+
+    public CourseReserve(String phone,String nickname,Member member,StatusCourse statusCourse,CourseMusic coursemusic){
+        this.phone = phone;
+        this.nickname = nickname;
+        this.member = member;
+        this.statusCourse = statusCourse;
+        this.coursemusic = coursemusic;
+    }
+
+}
