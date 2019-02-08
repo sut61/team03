68537072461973a29ec58f5baaca92384abe5b8b
U-DESKTOP-BKIDCO3\Jang_HYS <B5813575@g.sut.ac.th>
2019-02-09 03:27:55 +0700

    ทำการเพิ่ม Entity -close#28

---
+++ backend/src/main/java/com/sut/se/g03/entity/Staff.java
@@ -0,0 +1,36 @@
+
+package com.sut.se.g03.entity;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import javax.persistence.*;
+@Entity
+@ToString
+@EqualsAndHashCode
+public class Staff {
+    @Id
+    @SequenceGenerator(name = "staff_seq",sequenceName = "staff_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "staff_seq")
+    private Long id;
+    private String staffName;
+    public Staff(){}
+
+    public Staff(String staffName){
+        this.staffName = staffName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStaffName() {
+        return staffName;
+    }
+
+    public void setStaffName(String staffName) {
+        this.staffName = staffName;
+    }
+}
