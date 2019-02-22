ba00b2dd36c749f8fc2ac9113a206527de3a1d33
paripatdr <paripatwork@gmail.com>
2019-02-22 21:19:35 +0700

    close#129 - Insert Annonation

--- backend/src/main/java/com/sut/se/g03/entity/Cash.java
+++ backend/src/main/java/com/sut/se/g03/entity/Cash.java
@@ -5,6 +5,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
 
 @Entity
 @Getter
@@ -16,18 +20,27 @@ public class Cash {
     @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="cash_seq")
     @Id
     private  Long id;
+
+    @NotNull
+    @Size(min = 2 , max = 12)
+    @Pattern(regexp = "\\w+|[ก-๙]+")
     private  String cashreceive;
+
+    @PositiveOrZero
     private float cashprice;
 
     @ManyToOne(fetch = FetchType.EAGER,targetEntity = Member.class)
     @JoinColumn(name = "Member",insertable = true)
+    @NotNull
     private Member mem;
 
     @OneToOne
+    @NotNull
     private Bill bill;
 
 
     @ManyToOne(fetch = FetchType.EAGER,targetEntity = PaidStatus.class)
     @JoinColumn(name = "PaidStatus",insertable = true)
+    @NotNull
     private PaidStatus paidStatus;
 }
